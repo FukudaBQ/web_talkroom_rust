4dea92ea84db530db504009f644010df6c0963d2
Beau <bowei.qu@rakuten.com>
2025-02-07 17:30:20 +0800

    web api init && health check endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpServer, Responder};
+
+async fn health() -> impl Responder {
+    "1"
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/health", web::get().to(health))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
